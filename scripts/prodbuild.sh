e474f02035c48baa60202a937b85598450153faa
Cera Davies <ceralena.davies@gmail.com>
2017-04-24 20:34:47 +1000

    [CLEAN] remove docker stuff from prodbuild script

--- scripts/prodbuild.sh
+++ scripts/prodbuild.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -ue
 
-if [ $# -ne 1 ]; then
-  echo 'Usage: ./scripts/prodbuild.sh tag'
-  exit 1
-fi
-
-tag=$1
-name=cera/cera.id.au:$tag
-
 # backend
 yarn
 npm run prodbuild
@@ -16,19 +8,3 @@ npm run prodbuild
 # delete node_modules and install prod dependencies only
 rm -rf node_modules
 yarn --production --ignore-optional
-
-cat > run.sh <<EOF
-#!/bin/sh
-cd /var/www/cera.id.au
-export APP_VERSION=$tag
-export STATIC_DIR=\`pwd\`/static
-export NODE_ENV=production
-node dist/server/server.js
-EOF
-
-chmod 755 run.sh
-
-sudo docker build -t "$name" .
-git tag "$tag"
-
-echo "Done. After review, run:\n\tdocker push $name && git push origin $tag"
