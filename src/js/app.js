3efccd97fa1b472ad0afa56b1234a8ddf86be6a7
Cera Davies <ceralena.davies@gmail.com>
2020-07-18 17:58:11 +1000

    use jquery to add a blockquote class to all blockquote elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/app.css';
+
+import $ from 'jquery';
+
+$(document).ready(() => {
+  addStyling();
+});
+
+// hugo's syntax for adding attributes to elements generated from markdown is unfortunately quite awkward.
+//
+// This is a little workaround.
+function addStyling() {
+  // add the bootstrap blockquote class to all blockquote elements
+  $('blockquote').addClass('blockquote');
+}
