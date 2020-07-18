cae83c7054b5eb4156ba63ed9b02b02f66e9dd12
Cera Davies <ceralena.davies@gmail.com>
2020-07-18 15:53:21 +1000

    compile css to a seperate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+
   entry: {
     app: path.resolve('src', 'js', 'app.js'),
     style: path.resolve('src', 'js', 'style.js')
   },
+
   output: {
     path: path.resolve('static', 'assets'),
     filename: '[name].bundle.js',
   },
+
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
